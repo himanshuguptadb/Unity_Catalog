36dadaf31eb38a5f2aa03ad611a04be497e14a1b
himanshuguptadb <himanshuguptadb>
2023-03-30 12:40:47 +0000

    Empty_Database
    Added Logic to handle empty database

--- Metastore Sync Tables.py
+++ Metastore Sync Tables.py
@@ -5,7 +5,7 @@
 # COMMAND ----------
 
 from functools import reduce
-from pyspark.sql import DataFrame
+from pyspark.sql.functions import lit
 
 def sync_databases(database_to_upgrade, catalog_destination, database_destination = None, database_owner_to = None, dry_run = None):
   """Sync all tabes from one databse to the UC.
@@ -23,14 +23,28 @@ def sync_databases(database_to_upgrade, catalog_destination, database_destinatio
   spark.sql(f'CREATE DATABASE IF NOT EXISTS `{catalog_destination}`.`{database_destination}`')
   
   print(f'Upgrading database under `hive_metastore`.`{database_to_upgrade}` to UC database `{catalog_destination}`.`{database_destination}`.')
-  #Now iterate over all the table to synch them with the UC databse
-  if dry_run == "True":
-    sync_status = spark.sql(f"SYNC SCHEMA {catalog_destination}.{database_destination} FROM hive_metastore.{database_to_upgrade} SET OWNER `{database_owner_to}` DRY RUN").collect()
-  else:
-    sync_status = spark.sql(f"SYNC SCHEMA {catalog_destination}.{database_destination} FROM hive_metastore.{database_to_upgrade} SET OWNER `{database_owner_to}`").collect()
-  
   syncColumns = ["source_schema","source_name","source_type","target_catalog","target_schema","target_name","status_code","description"]
-  sync_statusDF = spark.createDataFrame(data=sync_status, schema = syncColumns)
+  syncColumns = StructType([
+  StructField('source_schema', StringType(), True),
+  StructField('source_name', StringType(), True),
+  StructField('source_type', StringType(), True),
+  StructField('target_catalog', StringType(), True),
+  StructField('target_schema', StringType(), True),
+  StructField('target_name', StringType(), True),
+  StructField('status_code', StringType(), True),
+  StructField('description', StringType(), True)
+  ])
+  
+  if len(spark.sql(f"SHOW TABLES IN hive_metastore.{database_to_upgrade}").collect()) != 0:
+    #Now iterate over all the table to synch them with the UC databse
+    if dry_run == "True":
+      sync_status = spark.sql(f"SYNC SCHEMA {catalog_destination}.{database_destination} FROM hive_metastore.{database_to_upgrade} SET OWNER `{database_owner_to}` DRY RUN").collect()
+    else:
+      sync_status = spark.sql(f"SYNC SCHEMA {catalog_destination}.{database_destination} FROM hive_metastore.{database_to_upgrade} SET OWNER `{database_owner_to}`").collect()
+  
+    sync_statusDF = spark.createDataFrame(data=sync_status, schema = syncColumns)
+  else:
+    sync_statusDF = spark.createDataFrame([(database_to_upgrade,"","",catalog_destination,database_destination,"","SUCESS","Empty Database")], schema = syncColumns)
 
   return sync_statusDF
 
@@ -54,7 +68,7 @@ from pyspark.sql.types import StructType,StructField, StringType
 
 print('Sync all external databases to the UC:')
 #databases = [row['databaseName'] for row in spark.sql(f"SHOW DATABASES IN hive_metastore").collect()]
-databases = ['himanshu_gupta_demo_hms']
+databases = ['testhg','himanshu_gupta_demo_hms']
 #upgrades 50 databases in parallel to speedup migration
 
 
